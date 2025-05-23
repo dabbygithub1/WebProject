9fe76f7ce89aba08ffd6bf124ea1e50b1206ec43
vladpashkevich88@gmail.com <145257376+dabbygithub1@users.noreply.github.com>
2025-05-23 22:08:31 +0300

    Изменение файла App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import AuthorizationPage from './pages/AuthorizationPage/AuthorizationPage';
+import ProfessionsPage from './pages/ProfessionsPage/ProfessionsPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/login" element={<AuthorizationPage />} />
+        <Route path="/professions" element={<ProfessionsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
