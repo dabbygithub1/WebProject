173250df86b84ffe5291322e09b8ff8a12c5dff7
vladpashkevich88@gmail.com <145257376+dabbygithub1@users.noreply.github.com>
2025-05-26 05:39:24 +0300

    Добавление БД (для редактирования/сохранения)

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,7 +1,10 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics"; 
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +14,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC-Phhj-ILB1MPvpFufB-RMSO7M0ogNJcI",
   authDomain: "calcora-42cf1.firebaseapp.com",
   projectId: "calcora-42cf1",
-  storageBucket: "calcora-42cf1.firebasestorage.app",
+  storageBucket: "calcora-42cf1.appspot.com", 
   messagingSenderId: "333413038995",
   appId: "1:333413038995:web:93af64d6871d787de78666",
   measurementId: "G-523QP4JLXQ"
@@ -19,5 +22,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app); 
+const auth = getAuth(app);  
+const db = getFirestore(app);
+
+export { auth, db }; 
